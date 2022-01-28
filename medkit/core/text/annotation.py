8624f506f416c72484c55a3e2c77a88d11c03786
HUYNH Kim-Tam <kim-tam.huynh@inria.fr>
2022-01-28 11:35:09 +0000

    Introduce Document/Annotation core classes and text core classes

---
+++ medkit/core/text/annotation.py
@@ -0,0 +1,36 @@
+__all__ = ["Entity", "Attribute", "Relation"]
+
+from medkit.core.annotation import Annotation
+
+
+class Entity(Annotation):
+    def __init__(self, label, spans, text):
+        super().__init__(label)
+        self.spans = spans
+        self.text = text
+
+    def __repr__(self):
+        annotation = super().__repr__()
+        return f"{annotation}, spans={self.spans!r}, text={self.text!r}"
+
+
+class Attribute(Annotation):
+    def __init__(self, label, target_id, value=None):
+        super().__init__(label)
+        self.target_id = target_id
+        self.value = value
+
+    def __repr__(self):
+        annotation = super().__repr__()
+        return f"{annotation}, target_id={self.target_id!r}, value={self.value}"
+
+
+class Relation(Annotation):
+    def __init__(self, label, source_id, target_id):
+        super().__init__(label)
+        self.source_id = source_id
+        self.target_id = target_id
+
+    def __repr__(self):
+        annotation = super.__repr__()
+        return f"{annotation}, source={self.source_id}, target_id={self.target_id}"
