a977d6fb39944d5e96e8a250577b7564c3a50898
Olivier Birot <olivier.birot@inria.fr>
2022-02-04 11:35:45 +0100

    Introduce AdditionalSpan class

--- medkit/core/text/span.py
+++ medkit/core/text/span.py
@@ -1,15 +1,44 @@
-__all__ = ["Span", "remove", "extract", "move"]
+__all__ = ["Span", "AdditionalSpan", "remove", "extract", "move"]
 
+import dataclasses
 from typing import List, NamedTuple, Tuple
 
 
 class Span(NamedTuple):
+    """
+    Slice of text extracted from the original text
+
+    Attributes
+    ----------
+    start: int
+        Index of the first character in the original text
+    end: int
+        Index of the last character in the original text, plus one
+    """
+
     start: int
     end: int
 
     @property
     def length(self):
         return self.end - self.start
+
+
+@dataclasses.dataclass
+class AdditionalSpan:
+    """
+    Slice of text not present in the original text
+
+    Attributes
+    ----------
+    length:
+        Number of characters
+    replaced_spans:
+        Slices of the original text that this span is replacing
+    """
+
+    length: int
+    replaced_spans: List[Span]
 
 
 def remove(
