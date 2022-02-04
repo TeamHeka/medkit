0ce63e55f8c9483146c576d08d31d48aea9ca229
Olivier Birot <olivier.birot@inria.fr>
2022-02-04 11:02:22 +0100

    Remove unused method in Span, add length property

--- medkit/core/text/span.py
+++ medkit/core/text/span.py
@@ -7,5 +7,6 @@ class Span(NamedTuple):
     start: int
     end: int
 
-    def contains(self, other_span):
-        return self.start <= other_span.start and self.end >= other_span.end
+    @property
+    def length(self):
+        return self.end - self.start
