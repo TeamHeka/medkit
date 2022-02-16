c7b0761a5d6c73a7810963eea174002d7349cd33
Olivier Birot <olivier.birot@inria.fr>
2022-02-16 11:20:51 +0100

    Add missing __init__.py in medkit/text/ner

---
+++ medkit/text/ner/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+    "RegexpMatcher",
+    "RegexpMatcherRule",
+    "RegexpMatcherNormalization",
+]
+
+from .regexp_matcher import RegexpMatcher, RegexpMatcherRule, RegexpMatcherNormalization
