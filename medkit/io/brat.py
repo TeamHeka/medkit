7cad65a2f6cf0a648331efefc4d25e64a413397d
Olivier Birot <olivier.birot@inria.fr>
2022-03-23 11:48:36 +0100

    In BratInputConverter, avoid "internal" wording
    Here we are talking about medkit Documents and Annotations as opposed
    to brat annotations. "Internal" means "private", which is not the case
    here.

--- medkit/io/brat.py
+++ medkit/io/brat.py
@@ -29,22 +29,21 @@ class BratInputConverter(InputConverter):
 
     def load(self, dir_path: str, text_extension: str) -> Collection:
         """
-        Load the documents and the brat annotations into internal collection of
-        documents
+        Create a Collection of TextDocuments from a folder containting text files
+        and associated brat annotations files (.ann).
 
         Parameters
         ----------
         dir_path: str
-            The path to the directory containing the text documents and the annotation
+            The path to the directory containing the text files and the annotation
             files (.ann)
         text_extension: str
-            The extension of the text document (e.g., .txt)
+            The extension of the text file (e.g., .txt)
 
         Returns
         -------
         Collection
-            The collection of documents (TextDocument)
-
+            The collection of TextDocuments
         """
         documents = list()
         dir_path = pathlib.Path(dir_path)
@@ -58,7 +57,7 @@ class BratInputConverter(InputConverter):
 
     def _load_file(self, text_path: str, ann_path: str) -> TextDocument:
         """
-        Internal method for loading an annotation file (.ann).
+        Create a TextDocument from text file and its associated annotation file (.ann)
 
         Parameters
         ----------
@@ -70,31 +69,30 @@ class BratInputConverter(InputConverter):
         Returns
         -------
         TextDocument
-            The internal Text Document
-
+            The document containing the text and the annotations
         """
         with open(text_path, encoding="utf-8") as text_file:
             text = text_file.read()
         filename = pathlib.Path(text_path).name
         metadata = {"name": filename}
-        internal_doc = TextDocument(text=text, metadata=metadata)
-        internal_doc.add_operation(self.description)
+        doc = TextDocument(text=text, metadata=metadata)
+        doc.add_operation(self.description)
         brat_doc = brat_utils.parse_file(ann_path)
         # First convert entities, then relations, finally attributes
         # because new annotation id is needed
         brat_ann = dict()
         for brat_entity in brat_doc.entities.values():
-            internal_entity = self._convert_brat_entity(brat_entity)
-            internal_doc.add_annotation(internal_entity)
-            brat_ann[brat_entity.id] = internal_entity
+            entity = self._convert_brat_entity(brat_entity)
+            doc.add_annotation(entity)
+            brat_ann[brat_entity.id] = entity
         for brat_relation in brat_doc.relations.values():
-            internal_relation = self._convert_brat_relation(brat_relation, brat_ann)
-            internal_doc.add_annotation(internal_relation)
-            brat_ann[brat_relation.id] = internal_relation
+            relation = self._convert_brat_relation(brat_relation, brat_ann)
+            doc.add_annotation(relation)
+            brat_ann[brat_relation.id] = relation
         for brat_attribute in brat_doc.attributes.values():
-            internal_attribute = self._convert_brat_attribute(brat_attribute)
-            brat_ann[brat_attribute.target].attrs.append(internal_attribute)
-        return internal_doc
+            attribute = self._convert_brat_attribute(brat_attribute)
+            brat_ann[brat_attribute.target].attrs.append(attribute)
+        return doc
 
     def _convert_brat_entity(self, brat_entity: brat_utils.Entity) -> Entity:
         return Entity(
