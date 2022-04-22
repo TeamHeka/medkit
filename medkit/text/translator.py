8065b9102511f41126f6edc7abec1fb51c3fbf5f
Olivier Birot <olivier.birot@inria.fr>
2022-04-22 15:39:54 +0200

    Translation + Spans POC

---
+++ medkit/text/translator.py
@@ -0,0 +1,238 @@
+from __future__ import annotations
+
+__all__ = ["Translator"]
+
+from collections import defaultdict
+from typing import List, Optional
+
+import torch
+import transformers
+
+from medkit.core import (
+    OperationDescription,
+    ProvBuilder,
+    generate_id,
+)
+
+from medkit.core.text import Segment, ModifiedSpan, span_utils
+
+
+class Translator:
+    def __init__(self, output_label: str = "translation", proc_id: str = None):
+        if proc_id is None:
+            proc_id = generate_id()
+
+        self.id = proc_id
+        self.output_label = output_label
+
+        self._translation_pipeline = transformers.pipeline(
+            "translation_en_to_fr", model="Helsinki-NLP/opus-mt-fr-en"
+        )
+        self._aligner = _Aligner()
+
+        self._prov_builder: Optional[ProvBuilder] = None
+
+    @property
+    def description(self) -> OperationDescription:
+        config = dict(
+            output_label=self.output_label,
+        )
+        return OperationDescription(
+            id=self.id, name=self.__class__.__name__, config=config
+        )
+
+    def set_prov_builder(self, prov_builder: ProvBuilder):
+        self._prov_builder = prov_builder
+
+    def run(self, segments: List[Segment]) -> List[Segment]:
+        return [self._translate_segment(segment) for segment in segments]
+
+    def _translate_segment(self, segment: Segment) -> Segment:
+        translated_text = self._translation_pipeline(segment.text)[0][
+            "translation_text"
+        ]
+        translated_spans = self._get_translated_spans(
+            translated_text, segment.text, segment.spans
+        )
+
+        translated_segment = Segment(
+            label=self.output_label,
+            spans=translated_spans,
+            text=translated_text,
+        )
+
+        if self._prov_builder is not None:
+            self._prov_builder.add_prov(
+                translated_segment, self.description, source_data_items=[segment]
+            )
+
+        return translated_segment
+
+    def _get_translated_spans(self, translated_text, original_text, original_spans):
+        alignment = self._aligner.align(translated_text, original_text)
+
+        translated_spans = []
+        current_char = 0
+        for translated_range, original_ranges in alignment.items():
+            translated_start, translated_end = translated_range
+            translated_sub_text = translated_text[translated_start:translated_end]
+
+            # handle gaps between aligned sub texts
+            if current_char < translated_start:
+                translated_spans.append(
+                    ModifiedSpan(translated_start - current_char, replaced_spans=[])
+                )
+
+            original_sub_text, original_sub_text_spans = span_utils.extract(
+                original_text, original_spans, original_ranges
+            )
+            if translated_sub_text == original_sub_text:
+                # if translation sub text is identical to original,
+                # we can use the original spans
+                translated_spans += original_sub_text_spans
+            else:
+                # otherwise create modified span pointing to original spans
+                length = translated_end - translated_start
+                translated_spans.append(
+                    ModifiedSpan(length, replaced_spans=original_sub_text_spans)
+                )
+
+            current_char = translated_end
+
+        # handle trail
+        if current_char < len(translated_text):
+            translated_spans.append(
+                ModifiedSpan(len(translated_text) - current_char, replaced_spans=[])
+            )
+
+        assert sum(s.length for s in translated_spans) == len(translated_text)
+        return translated_spans
+
+    @classmethod
+    def from_description(cls, description: OperationDescription):
+        return cls(proc_id=description.id, **description.config)
+
+
+class _Aligner:
+    def __init__(self):
+        self._model = transformers.BertModel.from_pretrained(
+            "bert-base-multilingual-cased"
+        )
+        self._tokenizer = transformers.BertTokenizerFast.from_pretrained(
+            "bert-base-multilingual-cased"
+        )
+
+    def align(self, source_text, target_text):
+        # preprocess
+        source_encoding = self._encode_text(source_text)
+        target_encoding = self._encode_text(target_text)
+        words_by_token_source = self._get_words_by_token(source_encoding)
+        words_by_token_target = self._get_words_by_token(target_encoding)
+
+        # align tokens
+        align_layer = 8
+        threshold = 1e-3
+        self._model.eval()
+        with torch.no_grad():
+            in_source = source_encoding["input_ids"]
+            out_source = self._model(in_source, output_hidden_states=True)
+            out_source = out_source[2][align_layer][0, 1:-1]
+            in_target = target_encoding["input_ids"]
+            out_target = self._model(in_target, output_hidden_states=True)
+            out_target = out_target[2][align_layer][0, 1:-1]
+
+            dot_prod = torch.matmul(out_source, out_target.transpose(-1, -2))
+
+            softmax_source_target = torch.nn.Softmax(dim=-1)(dot_prod)
+            softmax_target_source = torch.nn.Softmax(dim=-2)(dot_prod)
+
+            softmax_inter = (softmax_source_target > threshold) * (
+                softmax_target_source > threshold
+            )
+        tokens_alignments = torch.nonzero(softmax_inter, as_tuple=False)
+
+        # align word spans
+        alignments = defaultdict(list)
+        for source_token, target_token in tokens_alignments:
+            source_word = words_by_token_source[source_token]
+            source_range = tuple(source_encoding.word_to_chars(source_word))
+            target_word = words_by_token_target[target_token]
+            target_range = tuple(target_encoding.word_to_chars(target_word))
+            if target_range not in alignments[source_range]:
+                alignments[source_range].append(target_range)
+
+        alignments = self._fill_alignment_gaps(alignments, source_text, target_text)
+
+        return alignments
+
+    def _encode_text(self, text):
+        """Return a BatchEncoder instance
+        (useful for converting token back to words and CharSpans,
+        but requires a TokenizerFast)"""
+        encoding = self._tokenizer(
+            text,
+            return_tensors="pt",
+            truncation=True,
+            max_length=self._tokenizer.model_max_length,
+        )
+        return encoding
+
+    def _get_words_by_token(self, encoding):
+        """Return a list containing the word index for each token
+        (allows to map back each token to its corresponding word"""
+        nb_words = 0
+        words_by_token = []
+        prev_word = None
+        for word in encoding.word_ids():
+            if word is None:
+                continue
+            if word != prev_word:
+                nb_words += 1
+                prev_word = word
+            words_by_token.append(nb_words - 1)
+        return words_by_token
+
+    def _fill_alignment_gaps(self, alignments, source_text, target_text):
+        """Create additional alignments in gaps between existing alignments
+        when source and target text is identical in these gaps"""
+        current_char_in_source = 0
+        current_char_in_target = 0
+
+        new_alignments = {}
+
+        for source_range, target_ranges in alignments.items():
+            source_start, source_end = source_range
+            target_start = target_ranges[0][0]
+            assert target_start == min(r[0] for r in target_ranges)
+            target_end = target_ranges[-1][1]
+            assert target_end == max(r[1] for r in target_ranges)
+
+            if current_char_in_source < source_start:
+                source_sub_text_in_gap = source_text[
+                    current_char_in_source:source_start
+                ]
+                target_sub_text_in_gap = target_text[
+                    current_char_in_target:target_start
+                ]
+                if target_sub_text_in_gap == source_sub_text_in_gap:
+                    source_gap_range = (current_char_in_source, source_start)
+                    target_gap_range = (current_char_in_target, target_start)
+                    new_alignments[source_gap_range] = [target_gap_range]
+
+            current_char_in_source = source_end
+            current_char_in_target = target_end
+
+        # handle trail
+        if current_char_in_source < len(source_text) and current_char_in_target < len(
+            target_text
+        ):
+            source_text_trail = source_text[current_char_in_source:]
+            target_text_trail = target_text[current_char_in_target:]
+            if target_text_trail == source_text_trail:
+                source_trail_range = (current_char_in_source, len(source_text))
+                target_trail_range = (current_char_in_target, len(target_text))
+                new_alignments[source_trail_range] = [target_trail_range]
+
+        alignments.update(new_alignments)
+        alignments = dict(sorted(alignments.items()))
+        return alignments
---
+++ tests/text/test_translator.py
@@ -0,0 +1,77 @@
+import pytest
+
+from medkit.core.text import Segment, Span, ModifiedSpan, span_utils
+from medkit.text.translator import Translator
+from medkit.text.ner import RegexpMatcher, RegexpMatcherRule
+
+_TEST_DATA = [
+    # (
+    #     "Ma soeur est diabétique.",
+    #     "My sister is diabetic.",
+    #     [("My", "Ma"), ("sister", "soeur"), ("is", "est"), ("diabetic", "diabétique"),],
+    # ),
+    (
+        "Je souffre d'insuffisance cardiaque depuis 10 ans.",
+        "I've been suffering from heart failure for 10 years.",
+        [
+            ("I", "Je"),
+            ("suffering", "souffre"),
+            ("from", "d"),
+            ("heart", "cardiaque"),
+            ("failure", "insuffisance"),
+            ("for", "depuis"),
+            ("10", "10"),
+            ("years", "ans"),
+        ],
+    ),
+]
+
+
+@pytest.fixture(scope="module")
+def translator():
+    return Translator()
+
+
+@pytest.mark.parametrize(
+    "original_text,expected_translated_text,expected_word_alignments", _TEST_DATA
+)
+def test_translator(
+    translator, original_text, expected_translated_text, expected_word_alignments
+):
+    segment = Segment(
+        label="raw_text",
+        spans=[Span(0, len(original_text))],
+        text=original_text,
+    )
+
+    translated_segment = translator.run([segment])[0]
+    assert translated_segment.text == expected_translated_text
+
+    word_alignments = []
+    start = 0
+    for span in translated_segment.spans:
+        end = start + span.length
+        translated_sub_text = translated_segment.text[start:end]
+        if isinstance(span, ModifiedSpan):
+            original_sub_text = "".join(
+                original_text[s.start : s.end] for s in span.replaced_spans
+            )
+        else:
+            original_sub_text = original_text[span.start : span.end]
+        if any(c.isalnum() for c in original_sub_text):
+            word_alignments.append((translated_sub_text, original_sub_text))
+        start = end
+
+    assert word_alignments == expected_word_alignments
+
+    rule = RegexpMatcherRule(
+        regexp="heart failure",
+        label="heart failure",
+        id="id_heart_failure",
+        version="1",
+    )
+    matcher = RegexpMatcher(rules=[rule])
+    entity = matcher.run([translated_segment])[0]
+    spans = sorted(span_utils.normalize_spans(entity.spans))
+    entity_original_words = [original_text[s.start : s.end] for s in spans]
+    assert entity_original_words == ["insuffisance", "cardiaque"]
