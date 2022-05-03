878e3f1fe9d51083871701351139f2e67a7aae1c
Olivier Birot <olivier.birot@inria.fr>
2022-05-03 12:03:27 +0200

    Clean up Translator tests

--- tests/text/test_translator.py
+++ tests/text/test_translator.py
@@ -1,48 +1,34 @@
 import pytest
 
 from medkit.core.text import Segment, Span, ModifiedSpan, span_utils
-from medkit.text.translator import Translator
+from medkit.text.translator import Translator, _Aligner
 from medkit.text.ner import RegexpMatcher, RegexpMatcherRule
 
-_TEST_DATA = [
-    # (
-    #     "Ma soeur est diabétique.",
-    #     "My sister is diabetic.",
-    #     [("My", "Ma"), ("sister", "soeur"), ("is", "est"), ("diabetic", "diabétique"),],
-    # ),
-    (
-        "Je souffre d'insuffisance cardiaque depuis 10 ans.",
-        "I've been suffering from heart failure for 10 years.",
-        [
-            ("I", "Je"),
-            ("suffering", "souffre"),
-            ("from", "d"),
-            ("heart", "cardiaque"),
-            ("failure", "insuffisance"),
-            ("for", "depuis"),
-            ("10", "10"),
-            ("years", "ans"),
-        ],
-    ),
+
+_ORIGINAL_TEXT = "Je souffre d'insuffisance cardiaque depuis 10 ans."
+_TRANSLATED_TEXT = "I've been suffering from heart failure for 10 years."
+_TEXT_ALIGNMENTS = [
+    ("I", "Je"),
+    ("'ve been ", None),
+    ("suffering", "souffre"),
+    (" ", None),
+    ("from", "d"),
+    (" ", None),
+    ("heart", "cardiaque"),
+    (" ", None),
+    ("failure", "insuffisance"),
+    (" ", None),
+    ("for", "depuis"),
+    (" ", None),
+    ("10", "10"),
+    (" ", None),
+    ("years", "ans"),
+    (".", "."),
 ]
 
 
-def _get_word_alignments(original_text, translated_segment):
-    word_alignments = []
-    start = 0
-    for span in translated_segment.spans:
-        end = start + span.length
-        translated_sub_text = translated_segment.text[start:end]
-        if isinstance(span, ModifiedSpan):
-            original_sub_text = " ".join(
-                original_text[s.start : s.end] for s in span.replaced_spans
-            )
-        else:
-            original_sub_text = original_text[span.start : span.end]
-        if any(c.isalnum() for c in original_sub_text):
-            word_alignments.append((translated_sub_text, original_sub_text))
-        start = end
-    return word_alignments
+def _get_raw_text_segment(text=_ORIGINAL_TEXT):
+    return Segment(label="raw_text", spans=[Span(0, len(text))], text=text,)
 
 
 @pytest.fixture(scope="module")
@@ -50,24 +36,37 @@ def translator():
     return Translator()
 
 
-@pytest.mark.parametrize(
-    "original_text,expected_translated_text,expected_word_alignments", _TEST_DATA
-)
-def test_translator(
-    translator, original_text, expected_translated_text, expected_word_alignments
-):
-    segment = Segment(
-        label="raw_text",
-        spans=[Span(0, len(original_text))],
-        text=original_text,
-    )
-
+def test_translator(translator):
+    segment = _get_raw_text_segment(_ORIGINAL_TEXT)
     translated_segment = translator.run([segment])[0]
-    assert translated_segment.text == expected_translated_text
+    assert translated_segment.text == _TRANSLATED_TEXT
 
-    word_alignments = _get_word_alignments(original_text, translated_segment)
-    assert word_alignments == expected_word_alignments
 
+def _get_text_alignments(original_text, translated_segment):
+    """Return a list of tuple associated each word in the translated text
+    to its corresponding word in the original text (if any).
+    This is to visualize alignment in an easier way than with spans"""
+    text_alignments = []
+    start = 0
+    for span in translated_segment.spans:
+        end = start + span.length
+        translated_sub_text = translated_segment.text[start:end]
+        if isinstance(span, ModifiedSpan):
+            if span.replaced_spans:
+                original_sub_text = " ".join(
+                    original_text[s.start : s.end] for s in span.replaced_spans
+                )
+            else:
+                original_sub_text = None
+        else:
+            original_sub_text = original_text[span.start : span.end]
+        text_alignments.append((translated_sub_text, original_sub_text))
+        start = end
+    return text_alignments
+
+
+def test_translator_with_matcher(translator):
+    """Make sure we are able to link an entity matched on translated text back to original text"""
     rule = RegexpMatcherRule(
         regexp="heart failure",
         label="heart failure",
@@ -75,7 +74,19 @@ def test_translator(
         version="1",
     )
     matcher = RegexpMatcher(rules=[rule])
-    entity = matcher.run([translated_segment])[0]
+
+    segment = _get_raw_text_segment(_ORIGINAL_TEXT)
+    translated_segment = translator.run([segment])[0]
+
+    entities = matcher.run([translated_segment])
+    assert len(entities) == 1
+    entity = entities[0]
+    assert entity.label == "heart failure"
+    assert entity.text == "heart failure"
+
+    text_alignments = _get_text_alignments(_ORIGINAL_TEXT, translated_segment)
+    assert text_alignments == _TEXT_ALIGNMENTS
+
     spans = sorted(span_utils.normalize_spans(entity.spans))
-    entity_original_words = [original_text[s.start : s.end] for s in spans]
-    assert entity_original_words == ["insuffisance", "cardiaque"]
+    matched_original_text = " ".join(segment.text[s.start : s.end] for s in spans)
+    assert matched_original_text == "insuffisance cardiaque"
