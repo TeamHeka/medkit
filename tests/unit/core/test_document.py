988fb017fe05f27c995ff7457beddf0b9e6d074e
Olivier Birot <olivier.birot@inria.fr>
2022-06-13 11:22:49 +0200

    Add tests for core.Document base class

---
+++ tests/unit/core/test_document.py
@@ -0,0 +1,44 @@
+from medkit.core import Annotation
+from medkit.core.document import Document
+
+
+class _MockAnnotation(Annotation):
+    def __init__(self, label, value, keys=None, ann_id=None):
+        super().__init__(label=label, keys=keys, ann_id=ann_id)
+        self.value = value
+
+
+def test_basic():
+    "Basic usage, add annotations and retrieve them"
+
+    doc = Document()
+    ann_1 = _MockAnnotation("name", "Bob")
+    doc.add_annotation(ann_1)
+    ann_2 = _MockAnnotation("topic", "Cancer")
+    doc.add_annotation(ann_2)
+    ann_3 = _MockAnnotation("topic", "Chemotherapy")
+    doc.add_annotation(ann_3)
+
+    assert set(doc.get_annotations()) == {ann_1, ann_2, ann_3}
+    assert doc.get_annotation_by_id(ann_1.id) == ann_1
+    assert doc.get_annotations_by_label("name") == [ann_1]
+    assert doc.get_annotations_by_label("topic") == [ann_2, ann_3]
+    assert doc.get_annotations_by_label("misc") == []
+
+
+def test_keys():
+    """Retrieve annotations by keys"""
+
+    doc = Document()
+    ann_1 = _MockAnnotation("name", "Bob", keys=["names"])
+    doc.add_annotation(ann_1)
+    ann_2 = _MockAnnotation("topic", "Cancer", keys=["topics", "regexp_matches"])
+    doc.add_annotation(ann_2)
+    ann_3 = _MockAnnotation("topic", "Chemotherapy", keys=["topics", "umls_matches"])
+    doc.add_annotation(ann_3)
+
+    assert doc.get_annotations_by_key("names") == [ann_1]
+    assert doc.get_annotations_by_key("topics") == [ann_2, ann_3]
+    assert doc.get_annotations_by_key("regexp_matches") == [ann_2]
+    assert doc.get_annotations_by_key("umls_matches") == [ann_3]
+    assert doc.get_annotations_by_label("misc") == []
