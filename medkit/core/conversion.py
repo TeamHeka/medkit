9331287bee06fd30e905c337acd80a99529e9fb1
Camila A <deycy-camila.arias-villamil@inria.fr>
2022-07-06 18:20:01 +0200

    TBD:Update Input/Output Converter

--- medkit/core/conversion.py
+++ medkit/core/conversion.py
@@ -1,19 +1,23 @@
 import abc
+from typing import List, Optional, Union
 
-from medkit.core.document import Collection
+from medkit.core.document import Collection, Document
+from medkit.core.operation import Operation
 
 
-class InputConverter:
+class InputConverter(Operation):
     """Abstract class for converting external document to medkit documents"""
 
     @abc.abstractmethod
     def load(self, **kwargs) -> Collection:
-        pass
+        raise NotImplementedError
 
 
 class OutputConverter:
     """Abstract class for converting medkit document to external format"""
 
     @abc.abstractmethod
-    def save(self, collection: Collection):
-        pass
+    def convert(
+        self, docs: Union[List[Document], Collection], **kwargs
+    ) -> Optional[List]:
+        raise NotImplementedError
