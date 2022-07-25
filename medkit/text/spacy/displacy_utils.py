7ec40213838304a9adb78a271539424f3093e5ee
Olivier Birot <olivier.birot@inria.fr>
2022-07-25 14:37:14 +0200

    Introduce displacy helpers

---
+++ medkit/text/spacy/displacy_utils.py
@@ -0,0 +1,99 @@
+__all__ = ["medkit_doc_to_displacy", "entities_to_displacy"]
+
+from typing import Callable, Dict, List, Optional
+
+from medkit.core.text import TextDocument, Entity, span_utils
+
+
+def medkit_doc_to_displacy(
+    doc: TextDocument,
+    entity_labels: Optional[List[str]] = None,
+    entity_formatter: Optional[Callable[[Entity], str]] = None,
+    max_gap_length: int = 3,
+):
+    """Build data dict that can be passed to `displacy.render()`
+    (with params `manual=True` and `style="ent"`) from the entities of a document.
+
+    Parameters
+    ----------
+    doc:
+        Document to visualize.
+    entity_labels:
+        Labels of entities to display. If `None`, all entities are displayed
+    entity_formatter:
+        Optional function returning the text to display as label for a given entity.
+        If `None`, the entity label will be used.
+        Can be used for instance to display normalization information available
+        in entity attributes.
+    max_gap_length:
+        When cleaning up gaps in spans, spans around gaps smaller than `max_gap_length`
+        will be merged.
+        Cf :func:`~medkit.core.text.span_utils.clean_up_gaps_in_normalized_spans()`.
+    """
+    if doc.text is None:
+        raise RuntimeError(
+            "Document cannot be visualized with displacy because it doesn't contain its"
+            " full text"
+        )
+    if entity_labels:
+        entities = [
+            e for label in entity_labels for e in doc.get_annotations_by_label(label)
+        ]
+    else:
+        entities = doc.get_entities()
+
+    return entities_to_displacy(entities, doc.text, entity_formatter, max_gap_length)
+
+
+def entities_to_displacy(
+    entities: List[Entity],
+    raw_text: str,
+    entity_formatter: Optional[Callable[[Entity], str]] = None,
+    max_gap_length: int = 3,
+) -> Dict:
+    """Build data dict that can be passed to `displacy.render()`
+    (with params `manual=True` and `style="ent"`) from list of entities
+
+    Parameters
+    ----------
+    entities:
+        Entities to visualize in text context.
+    raw_text:
+        Initial document text from which entities where extracted and to which they spans refer
+        (typically the `text` attribute of a :class:`~medkit.core.text.document.TextDocument`).
+    entity_formatter:
+        Optional function returning the text to display as label for a given entity.
+        If `None`, the entity label will be used.
+        Can be used for instance to display normalization information available
+        in entity attributes.
+    max_gap_length:
+        When cleaning up gaps in spans, spans around gaps smaller than `max_gap_length`
+        will be merged.
+        Cf :func:`~medkit.core.text.span_utils.clean_up_gaps_in_normalized_spans()`.
+    """
+    entities_data = []
+
+    for entity in entities:
+        normalized_spans = span_utils.normalize_spans(entity.spans)
+        # normalized spans can be empty if spans contained ModifiedSpan with no replaced_spans
+        if not normalized_spans:
+            continue
+
+        # merge close spans
+        cleaned_spans = span_utils.clean_up_gaps_in_normalized_spans(
+            normalized_spans, raw_text, max_gap_length=max_gap_length
+        )
+
+        # generate text label
+        if entity_formatter:
+            label = entity_formatter(entity)
+        else:
+            label = entity.label
+
+        entities_data += [
+            {"start": span.start, "end": span.end, "label": label}
+            for span in cleaned_spans
+        ]
+
+    entities_data = sorted(entities_data, key=lambda d: d["start"])
+    return {"text": raw_text, "ents": entities_data}
