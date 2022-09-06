5edaedfb956a1946b9d21e2e0f6611a7547910a0
Camila A <deycy-camila.arias-villamil@inria.fr>
2022-09-06 17:09:50 +0200

    Refactor AttributePropagator

--- medkit/text/postprocessing/propagator.py
+++ medkit/text/postprocessing/propagator.py
@@ -2,11 +2,10 @@ from __future__ import annotations
 
 __all__ = ["AttributePropagator"]
 
-from typing import List, Optional
+from typing import Dict, List, Optional
 from intervaltree import IntervalTree
 
 from medkit.core.text import Segment, span_utils, ContextOperation
-from medkit.core import Attribute
 
 
 class AttributePropagator(ContextOperation):
@@ -15,7 +14,6 @@ class AttributePropagator(ContextOperation):
     def __init__(
         self,
         attr_labels: List[str],
-        max_gap_length: int = 3,
         op_id: Optional[str] = None,
     ):
         """Instantiate the attribute propagator
@@ -24,71 +22,64 @@ class AttributePropagator(ContextOperation):
         ----------
         attr_labels:
             List of the labels of the attributes to propagate
-        max_gap_length:
-            When cleaning up gaps in spans, spans around gaps smaller than `max_gap_length`
-            will be merged.
-            Cf :func:`~medkit.core.text.span_utils.clean_up_gaps_in_normalized_spans()`.
         op_id:
             Identifier of the detector
         """
-
         self.attr_labels = attr_labels
-        self.max_gap_length = max_gap_length
-
         # Pass all arguments to super (remove self)
         init_args = locals()
         init_args.pop("self")
         super().__init__(**init_args)
 
-    def run(self, segments: List[Segment]):
-        """Add attributes from a segment to all nested segments
+    def run(self, source_segments: List[Segment], target_segments: List[Segment]):
+        """Add attributes from a source segment to all nested segments
 
         Parameters
         ----------
-        segments:
-            List of segments to be included in the propagation operation (sources and targets)
+        source_segments:
+            List of segments with attributes to propagate
+        target_segments:
+            List of segments target
         """
+        nested = self._compute_nested_segments(source_segments, target_segments)
 
-        nested = self._compute_nested_segments(segments)
-
-        for parent in nested.keys():
+        for parent, children in nested.items():
             attrs_to_propagate = [
                 attr
-                for attr in parent.data.get_attrs()
-                if attr.label in self.attr_labels
+                for label in self.attr_labels
+                for attr in parent.get_attrs_by_label(label)
             ]
 
             if len(attrs_to_propagate) == 0:
                 continue
 
+            # copy the attribute from source to target
             for attr in attrs_to_propagate:
-                for child in nested[parent]:
-                    self._propagate_attribute(target=child.data, attr=attr)
+                for child in children:
+                    child.add_attr(attr)
 
-    def _compute_nested_segments(self, segments: Segment) -> IntervalTree:
-        t = IntervalTree()
-
-        for segment in segments:
+    def _compute_nested_segments(
+        self, source_segments, target_segments: Segment
+    ) -> IntervalTree:
+        tree = IntervalTree()
+        for segment in target_segments:
             normalized_spans = span_utils.normalize_spans(segment.spans)
+
             if not normalized_spans:
                 continue
-
-            t.addi(
-                min([s.start for s in normalized_spans]),
-                max([s.end for s in normalized_spans]),
+            tree.addi(
+                normalized_spans[0].start,
+                normalized_spans[-1].end,
                 data=segment,
             )
+        return self._find_nested(tree, source_segments)
 
-        return t.find_nested()
-
-    def _propagate_attribute(self, target: Segment, attr: Attribute):
-        target_attr = Attribute(
-            label=attr.label, value=attr.value, metadata=attr.metadata
-        )
-
-        if self._prov_tracer is not None:
-            self._prov_tracer.add_prov(
-                target_attr, self.description, source_data_items=[attr]
-            )
-
-        target.add_attr(target_attr)
+    def _find_nested(
+        self, tree: IntervalTree, source_segments: List[Segment]
+    ) -> Dict[Segment, List[Segment]]:
+        nested = {}
+        for segment in source_segments:
+            normalized_spans = span_utils.normalize_spans(segment.spans)
+            start, end = normalized_spans[0].start, normalized_spans[-1].end
+            nested[segment] = [child.data for child in tree.overlap(start, end)]
+        return nested
