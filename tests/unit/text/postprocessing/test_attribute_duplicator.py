71caa170fec18d0085d6bbcabcb9aac4ba378a0e
Camila A <deycy-camila.arias-villamil@inria.fr>
2022-10-19 18:25:32 +0200

    Simplify unit test of provenance

--- tests/unit/text/postprocessing/test_attribute_duplicator.py
+++ tests/unit/text/postprocessing/test_attribute_duplicator.py
@@ -103,31 +103,19 @@ def test_duplicate_several_attrs():
 def test_provenance():
     doc = _get_doc()
     sentences = doc.get_annotations_by_label("sentence")
-    syntagmes = doc.get_annotations_by_label("syntagme")
     targets = doc.get_annotations_by_label("disease")
 
     prov_tracer = ProvTracer()
     propagator_1 = AttributeDuplicator(attr_labels=["is_family"])
-    propagator_2 = AttributeDuplicator(attr_labels=["is_negated"])
     propagator_1.set_prov_tracer(prov_tracer)
-    propagator_2.set_prov_tracer(prov_tracer)
 
     # is_family was 'detected' in sentences
     propagator_1.run(sentences, targets)
-    # is_negated was 'detected' in syntagmes
-    propagator_2.run(syntagmes, targets)
 
     sentence_attr_1 = sentences[0].get_attrs_by_label("is_family")[0]
-    syntagme_attr_2 = syntagmes[0].get_attrs_by_label("is_negated")[0]
     attr_1 = targets[0].get_attrs_by_label("is_family")[0]
-    attr_2 = targets[0].get_attrs_by_label("is_negated")[0]
 
     attr_1_prov = prov_tracer.get_prov(attr_1.id)
     assert attr_1_prov.data_item == attr_1
     assert attr_1_prov.op_desc == propagator_1.description
     assert attr_1_prov.source_data_items == [sentence_attr_1]
-
-    attr_2_prov = prov_tracer.get_prov(attr_2.id)
-    assert attr_2_prov.data_item == attr_2
-    assert attr_2_prov.op_desc == propagator_2.description
-    assert attr_2_prov.source_data_items == [syntagme_attr_2]
