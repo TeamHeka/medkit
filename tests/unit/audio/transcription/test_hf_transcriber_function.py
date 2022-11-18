4916b37870c22e9bc239483a890e097c2668406e
Olivier Birot <olivier.birot@inria.fr>
2022-11-18 14:15:39 +0100

    Use mock pipeline in HFTranscriberFunction unit tests
    These tests are very close to the SBTrancriberFunction unit tests.

--- tests/unit/audio/transcription/test_hf_transcriber_function.py
+++ tests/unit/audio/transcription/test_hf_transcriber_function.py
@@ -5,50 +5,59 @@ pytest.importorskip(modname="transformers", reason="transformers is not installe
 
 import numpy as np  # noqa: E402
 
-from medkit.core.audio import FileAudioBuffer, MemoryAudioBuffer  # noqa: E402
-from medkit.audio.transcription.hf_transcriber_function import (  # noqa: E402
-    HFTranscriberFunction,
-)
+from medkit.core.audio import MemoryAudioBuffer  # noqa: E402
+from medkit.audio.transcription import HFTranscriberFunction  # noqa: E402
 
 
-_AUDIO = FileAudioBuffer("tests/data/audio/voice.ogg")
-_TEXT = "Hello this is my voice i am speaking to you."
+_SAMPLE_RATE = 16000
+_TEXT_TEMPLATE = "AUDIO HAS {} SAMPLES"
 
 
-@pytest.fixture(scope="module")
-def transcriber_func():
-    return HFTranscriberFunction()
+# mock of AutomaticSpeechRecognitionPipeline class used by HFTranscriberFunction
+class _MockPipeline:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, audio_dicts):
+        # for each audio dict in batch, return string containing sample count
+        # (allows us to check that each audio input has corresponding output)
+        text_dicts = []
+        for audio_dict in audio_dicts:
+            nb_samples = int(audio_dict["raw"].shape[-1])
+            text = _TEXT_TEMPLATE.format(nb_samples)
+            text_dicts.append({"text": text})
+
+        return text_dicts
 
 
-@pytest.fixture(scope="module")
-def transcriber_func_no_dot_no_capitalize():
-    return HFTranscriberFunction(add_trailing_dot=False, capitalize=False)
+@pytest.fixture(scope="module", autouse=True)
+def _mocked_pipeline(module_mocker):
+    module_mocker.patch(
+        "transformers.pipelines.get_task", lambda m: "automatic-speech-recognition"
+    )
+    module_mocker.patch("transformers.pipeline", _MockPipeline)
 
 
-def test_basic(transcriber_func):
-    text = transcriber_func.transcribe([_AUDIO])[0]
-    assert text == _TEXT
+def _gen_audio(nb_samples):
+    return MemoryAudioBuffer(signal=np.zeros((1, nb_samples)), sample_rate=_SAMPLE_RATE)
 
 
-def test_no_dot_no_capitalize(transcriber_func_no_dot_no_capitalize):
-    text = transcriber_func_no_dot_no_capitalize.transcribe([_AUDIO])[0]
-    assert text == _TEXT.lower()[:-1]
+def test_basic():
+    """Basic behavior"""
+
+    transcriber_func = HFTranscriberFunction(model="mock-model")
+    texts = transcriber_func.transcribe([_gen_audio(1000), _gen_audio(2000)])
+    assert texts == ["Audio has 1000 samples.", "Audio has 2000 samples."]
 
 
-def test_batch(transcriber_func):
-    # generate batch of different audios by duplicating signal every other time
-    audios = []
-    short_signal = _AUDIO.read()
-    long_signal = np.concatenate((short_signal, short_signal), axis=1)
-    for i in range(10):
-        signal = short_signal if i % 2 else long_signal
-        audio = MemoryAudioBuffer(signal, _AUDIO.sample_rate)
-        audios.append(audio)
+def test_no_formatting():
+    """No reformatting of transcribed text (raw text as returned by transformers pipeline)
+    """
 
-    # transcribe batch of audios
-    texts = transcriber_func.transcribe(audios)
-    assert len(texts) == len(audios)
-    # check that result is identical to transcribing one by one
-    for audio, text in zip(audios, texts):
-        expected_text = transcriber_func.transcribe([audio])[0]
-        assert text == expected_text
+    transcriber_func = HFTranscriberFunction(
+        model="mock-model",
+        add_trailing_dot=False,
+        capitalize=False,
+    )
+    texts = transcriber_func.transcribe([_gen_audio(1000)])
+    assert texts == ["AUDIO HAS 1000 SAMPLES"]
