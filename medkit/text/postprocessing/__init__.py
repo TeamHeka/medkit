a35f6b6b26671f6098ea1fddc4c99f93678d5941
Camila A <deycy-camila.arias-villamil@inria.fr>
2023-04-19 14:29:12 +0200

    Add alignments utils using IntervalTree

--- medkit/text/postprocessing/__init__.py
+++ medkit/text/postprocessing/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["AttributeDuplicator"]
+__all__ = ["AttributeDuplicator", "create_segments_tree", "compute_nested_segments"]
 
+from .alignment_utils import compute_nested_segments, create_segments_tree
 from .attribute_duplicator import AttributeDuplicator
