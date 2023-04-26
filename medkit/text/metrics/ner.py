0b4fdcfb309e1038ab45fb2d7686af283d0fe4fd
Camila A <deycy-camila.arias-villamil@inria.fr>
2023-04-26 19:12:13 +0200

    Use by default a tokenizer per character in SeqEvaEvaluator

--- medkit/text/metrics/ner.py
+++ medkit/text/metrics/ner.py
@@ -1,14 +1,15 @@
-__all__ = ["SeqEvalMetricsComputer", "SeqEvalEvaluator"]
+__all__ = ["SeqEvalEvaluator", "SeqEvalMetricsComputer"]
 
+import dataclasses
 import itertools
-from typing import Dict, List, Literal
+from typing import Any, Dict, List, Optional
+from typing_extensions import Literal
 
 from seqeval.metrics import accuracy_score, classification_report
 from seqeval.scheme import BILOU, IOB2
 
 from medkit.core.text import TextDocument, Entity
 from medkit.text.ner import hf_tokenization_utils
-
 from medkit.training.utils import BatchData
 
 SPECIAL_TAG_ID_HF: int = -100
@@ -57,6 +58,147 @@ def _compute_seqeval_from_dict(
     return scores
 
 
+class SeqEvalEvaluator:
+    """Evaluator to compute the performance of labeling tasks such as
+    named entity recognition. This evaluator compares TextDocuments of reference
+    with its predicted annotations and returns a dictionary of metrics.
+
+    The evaluator converts the set of entites and documents to tags before compute the metric.
+    It supports two schemes, IOB2 (a BIO scheme) and BILOU. The IOB2 scheme tags the Beginning,
+    the Inside and the Outside text of a entity. The BILOU scheme tags the Beginning,
+    the Inside and the Last tokens of multi-token entity as well as Unit-length entity.
+
+    For more information about IOB schemes, refer to the `Wikipedia page <https://en.wikipedia.org/wiki/Inside%E2%80%93outside%E2%80%93beginning_(tagging)>`_
+
+    (TODO: cite in documentation, add example here or in `compute`)
+    Hiroki Nakayama. (2018). seqeval: A Python framework for sequence labeling evaluation.
+
+    .. hint::
+        If **tokenizer** is not defined, the evaluator tokenizes the text by character.
+        This may generate a lot of tokens with large documents and may affect execution time.
+        You can use a fast tokenizer from HuggingFace, i.e. : bert tokenizer
+
+        >>> from transformers import AutoTokenizer
+        >>> tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
+    """
+
+    def __init__(
+        self,
+        tokenizer: Optional[Any] = None,
+        tagging_scheme: Literal["bilou", "iob2"] = "bilou",
+        return_metrics_by_label: bool = True,
+    ):
+        """
+        Parameters
+        ----------
+        tokenizer:
+            Optional tokenizer to convert text into tokens.
+            If not provided, a tokenizer per-character is created.
+        tagging_scheme:
+            Scheme for tagging the tokens, it can be `bilou` or `iob2`
+        return_metrics_by_label:
+            If `True`, return the metrics by label in the output dictionnary.
+            If `False`, only return overall metrics
+        """
+        if tokenizer is None:
+            tokenizer = _CharacterTokenizer()
+
+        self.tokenizer = tokenizer
+        self.tagging_scheme = tagging_scheme
+        self.return_metrics_by_label = return_metrics_by_label
+
+        # internal configuration for seqeval, 'bilou' only works with 'strict' mode
+        self._scheme = BILOU if self.tagging_scheme == "bilou" else IOB2
+        self._mode = "strict" if self.tagging_scheme == "bilou" else None
+
+    def compute(
+        self, documents: List[TextDocument], predicted_entities: List[List[Entity]]
+    ) -> Dict[str, float]:
+        """Compute metrics of entity matching giving predictions.
+
+        Parameters
+        ----------
+        documents:
+            Text documents containing entities of reference
+        predicted_entities:
+            List of predicted entities by document
+
+        Returns
+        -------
+        Dict[str, float]:
+            A dictionary with overall and per type metrics if required. The metrics included are:
+            accuracy, precision, recall and F1 score.
+        """
+        true_tags_all, pred_tags_all = [], []
+
+        for document, pred_entities in zip(documents, predicted_entities):
+            text_encoding = self.tokenizer(document.text).encodings[0]
+            true_entities = document.anns.entities
+
+            true_tags_all.append(
+                hf_tokenization_utils.transform_entities_to_tags(
+                    text_encoding=text_encoding,
+                    entities=true_entities,
+                    tagging_scheme=self.tagging_scheme,
+                )
+            )
+            pred_tags_all.append(
+                hf_tokenization_utils.transform_entities_to_tags(
+                    text_encoding=text_encoding,
+                    entities=pred_entities,
+                    tagging_scheme=self.tagging_scheme,
+                )
+            )
+
+        all_data = {"y_true": true_tags_all, "y_pred": pred_tags_all}
+
+        scores = _compute_seqeval_from_dict(
+            all_data=all_data,
+            mode=self._mode,
+            scheme=self._scheme,
+            return_metrics_by_label=self.return_metrics_by_label,
+        )
+        return scores
+
+
+class _CharacterTokenizer:
+    """A simple implementation of a per-character tokenizer.
+    The outputs use the same structure as a HuggingFace tokenizer,
+    this guarantees compatibility with tokenization methods.
+
+    Example
+    -------
+    >>> tokenizer = CharacterTokenizer()
+    >>> tokenizer("hello")
+    BatchEncoding(encodings=[Encoding(tokens=['h', 'e', 'l', 'l', 'o'])])
+    """
+
+    def __call__(self, text: str):
+        return _BatchEncoding([_Encoding(tokens=[c for c in text])])
+
+
+@dataclasses.dataclass
+class _Encoding:
+    tokens: List[str]
+
+    def char_to_token(self, idx):
+        return idx
+
+    def __len__(self):
+        return len(self.tokens)
+
+
+@dataclasses.dataclass
+class _BatchEncoding:
+    encodings: List[_Encoding]
+
+    def __len__(self):
+        return len(self.encodings)
+
+    def __getitem__(self, idx):
+        return self.encodings[idx]
+
+
 class SeqEvalMetricsComputer:
     """An implementation of :class:`~medkit.training.MetricsComputer` using seqeval
     to compute metrics in the training of named-entity recognition components.
@@ -80,9 +222,11 @@ class SeqEvalMetricsComputer:
             If `False`, only return overall metrics
         """
         self.id_to_label = id_to_label
-        self.scheme = BILOU if tagging_scheme == "bilou" else IOB2
         self.return_metrics_by_label = return_metrics_by_label
-        # bilou only works in strict mode
+
+        # internal configuration for seqeval,
+        # 'bilou' only works with 'strict' mode
+        self._scheme = BILOU if tagging_scheme == "bilou" else IOB2
         self._mode = "strict" if tagging_scheme == "bilou" else None
 
     def prepare_batch(
@@ -139,107 +283,7 @@ class SeqEvalMetricsComputer:
         scores = _compute_seqeval_from_dict(
             all_data=all_data,
             mode=self._mode,
-            scheme=self.scheme,
+            scheme=self._scheme,
             return_metrics_by_label=self.return_metrics_by_label,
         )
         return scores
-
-
-class SeqEvalEvaluator:
-    """Evaluator that use seqeval to compute the performance of labelisation tasks
-    such as named-entity recognition. This evaluator compares TextDocuments of reference
-    with its predicted annotations and returns a dictionary of metrics.
-
-    The evaluator converts the set of entites and documents to tags before compute the metric.
-    It supports two schemes, IOB2 (a BIO scheme) and BILOU. The IOB2 scheme tags the Beginning,
-    the Inside and the Outside text of a entity. The BILOU scheme tags the Beginning,
-    the Inside and the Last tokens of multi-token entity as well as Unit-length entity.
-
-    For more information about IOB schemes, refer to the `Wikipedia page <https://en.wikipedia.org/wiki/Inside%E2%80%93outside%E2%80%93beginning_(tagging)>`_
-
-    (TODO: cite in documentation, add example here or in `compute`)
-    Hiroki Nakayama. (2018). seqeval: A Python framework for sequence labeling evaluation.
-    """
-
-    def __init__(
-        self,
-        tokenizer,
-        tagging_scheme: Literal["bilou", "iob2"] = "bilou",
-        return_metrics_by_label: bool = True,
-    ):
-        """
-        Parameters
-        ----------
-        tokenizer:
-            A fast HuggingFace tokenizer to convert text into tokens
-        tagging_scheme:
-            Scheme for tagging the tokens, it can be `bilou` or `iob2`
-        return_metrics_by_label:
-            If `True`, return the metrics by label in the output dictionnary.
-            If `False`, only return overall metrics
-        """
-        self.tokenizer = tokenizer
-        self.tagging_scheme = tagging_scheme
-        self.scheme = BILOU if self.tagging_scheme == "bilou" else IOB2
-        self.return_metrics_by_label = return_metrics_by_label
-        # bilou only works in strict mode
-        self._mode = "strict" if self.tagging_scheme == "bilou" else None
-
-    def compute(
-        self, documents: List[TextDocument], predicted_entities: List[List[Entity]]
-    ) -> Dict[str, float]:
-        """Compute metrics of entity matching giving predictions.
-
-        Parameters
-        ----------
-        documents:
-            Text documents containing entities of reference
-        predicted_entities:
-            List of predicted entities by document
-
-        Returns
-        -------
-        Dict[str, float]:
-            A dictionary with overall and per type metrics if required. The metrics included are:
-            accuracy, precision, recall and F1 score.
-        """
-        true_tags_all, pred_tags_all = [], []
-
-        for document, pred_entities in zip(documents, predicted_entities):
-            text_encoding = self._encode_text(document.text)
-            true_entities = document.anns.entities
-
-            true_tags_all.append(
-                hf_tokenization_utils.transform_entities_to_tags(
-                    text_encoding=text_encoding,
-                    entities=true_entities,
-                    tagging_scheme=self.tagging_scheme,
-                )
-            )
-            pred_tags_all.append(
-                hf_tokenization_utils.transform_entities_to_tags(
-                    text_encoding=text_encoding,
-                    entities=pred_entities,
-                    tagging_scheme=self.tagging_scheme,
-                )
-            )
-
-        all_data = {"y_true": true_tags_all, "y_pred": pred_tags_all}
-
-        scores = _compute_seqeval_from_dict(
-            all_data=all_data,
-            mode=self._mode,
-            scheme=self.scheme,
-            return_metrics_by_label=self.return_metrics_by_label,
-        )
-        return scores
-
-    def _encode_text(self, text):
-        """Return a EncodingFast instance"""
-        text_tokenized = self.tokenizer(
-            text,
-            truncation=True,
-            return_special_tokens_mask=True,
-        )
-        encoding = text_tokenized.encodings[0]
-        return encoding
