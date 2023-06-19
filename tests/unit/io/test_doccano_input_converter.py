b6ccf7eff43d1588bb1aaca88487149f53834abd
Camila A <deycy-camila.arias-villamil@inria.fr>
2023-06-19 14:29:34 +0200

    Test prov and refactor tests in doccano

--- tests/unit/io/test_doccano_input_converter.py
+++ tests/unit/io/test_doccano_input_converter.py
@@ -1,7 +1,29 @@
 import json
 from zipfile import ZipFile
 
-from medkit.io import DoccanoTask, DoccanoInputConverter
+import pytest
+
+from medkit.core.prov_tracer import ProvTracer
+from medkit.io import DoccanoInputConverter, DoccanoTask
+
+TEST_LINE_BY_TASK = {
+    DoccanoTask.RELATION_EXTRACTION: {
+        "text": "medkit was created in 2022",
+        "entities": [
+            {"id": 0, "start_offset": 0, "end_offset": 6, "label": "ORG"},
+            {"id": 1, "start_offset": 22, "end_offset": 26, "label": "DATE"},
+        ],
+        "relations": [{"id": 0, "from_id": 0, "to_id": 1, "type": "created_in"}],
+    },
+    DoccanoTask.SEQUENCE_LABELING: {
+        "text": "medkit was created in 2022",
+        "label": [(0, 6, "ORG"), (22, 26, "DATE")],
+    },
+    DoccanoTask.TEXT_CLASSIFICATION: {
+        "text": "medkit was created in 2022",
+        "label": ["header"],
+    },
+}
 
 
 def create_doccano_files_disk(tmp_path, data, task):
@@ -13,19 +35,12 @@ def create_doccano_files_disk(tmp_path, data, task):
 
 
 def test_relation_extraction_converter(tmp_path):
-    test_line = {
-        "text": "medkit was created in 2022",
-        "entities": [
-            {"id": 0, "start_offset": 0, "end_offset": 6, "label": "ORG"},
-            {"id": 1, "start_offset": 22, "end_offset": 26, "label": "DATE"},
-        ],
-        "relations": [{"id": 0, "from_id": 0, "to_id": 1, "type": "created_in"}],
-    }
-    # prepare zip file
-    task = "relation"
-    create_doccano_files_disk(tmp_path, data=test_line, task=task)
-    converter = DoccanoInputConverter(task=DoccanoTask.RELATION_EXTRACTION)
-    documents = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task}")
+    task = DoccanoTask.RELATION_EXTRACTION
+    test_line = TEST_LINE_BY_TASK[task]
+    create_doccano_files_disk(tmp_path, data=test_line, task=task.value)
+
+    converter = DoccanoInputConverter(task=task)
+    documents = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task.value}")
     assert len(documents) == 1
 
     document = documents[0]
@@ -43,15 +58,12 @@ def test_relation_extraction_converter(tmp_path):
 
 
 def test_sequence_labeling_converter(tmp_path):
-    test_line = {
-        "text": "medkit was created in 2022",
-        "label": [(0, 6, "ORG"), (22, 26, "DATE")],
-    }
-    # prepare zip file
-    task = "sequence_labeling"
-    create_doccano_files_disk(tmp_path, data=test_line, task=task)
-    converter = DoccanoInputConverter(task=DoccanoTask.SEQUENCE_LABELING)
-    documents = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task}")
+    task = DoccanoTask.SEQUENCE_LABELING
+    test_line = TEST_LINE_BY_TASK[task]
+    create_doccano_files_disk(tmp_path, data=test_line, task=task.value)
+
+    converter = DoccanoInputConverter(task=task)
+    documents = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task.value}")
     assert len(documents) == 1
 
     document = documents[0]
@@ -63,13 +75,12 @@ def test_sequence_labeling_converter(tmp_path):
 
 
 def test_text_classification_converter(tmp_path):
-    test_line = {"text": "medkit was created in 2022", "label": ["header"]}
-    # prepare zip file
-    task = "text_classification"
-    create_doccano_files_disk(tmp_path, data=test_line, task=task)
+    task = DoccanoTask.TEXT_CLASSIFICATION
+    test_line = TEST_LINE_BY_TASK[task]
+    create_doccano_files_disk(tmp_path, data=test_line, task=task.value)
 
-    converter = DoccanoInputConverter(task=DoccanoTask.TEXT_CLASSIFICATION)
-    documents = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task}")
+    converter = DoccanoInputConverter(task=task)
+    documents = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task.value}")
     assert len(documents) == 1
 
     document = documents[0]
@@ -81,3 +92,31 @@ def test_text_classification_converter(tmp_path):
     attrs = segment.attrs.get(label=expected_label)
     assert len(attrs) == 1
     assert attrs[0].value == "header"
+
+
+TEST_PROV_BY_TASK = [
+    (DoccanoTask.RELATION_EXTRACTION, True),
+    (DoccanoTask.SEQUENCE_LABELING, True),
+    (DoccanoTask.TEXT_CLASSIFICATION, False),
+]
+
+
+@pytest.mark.parametrize(
+    "task,check_prov_entity",
+    TEST_PROV_BY_TASK,
+)
+def test_prov(tmp_path, task, check_prov_entity):
+    converter = DoccanoInputConverter(task=task)
+    prov_tracer = ProvTracer()
+    converter.set_prov_tracer(prov_tracer)
+
+    test_line = TEST_LINE_BY_TASK[task]
+    create_doccano_files_disk(tmp_path, data=test_line, task=task.value)
+    docs = converter.load_from_directory_zip(dir_path=f"{tmp_path}/{task.value}")
+
+    doc = docs[0]
+    ann = doc.anns.entities[0] if check_prov_entity else doc.raw_segment.attrs.get()[0]
+    prov = prov_tracer.get_prov(ann.uid)
+    assert prov.data_item == ann
+    assert prov.op_desc == converter.description
+    assert len(prov.source_data_items) == 0
