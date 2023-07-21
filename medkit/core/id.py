e2e228c93b63cc4b611d66a41f536fb3017aaaaa
Camila A <deycy-camila.arias-villamil@inria.fr>
2023-07-21 15:26:17 +0200

    [core] update docstring for uid function

--- medkit/core/id.py
+++ medkit/core/id.py
@@ -8,9 +8,20 @@ def generate_id() -> str:
     return str(uuid.uuid1())
 
 
-def generate_deterministic_id(reference_id: str):
-    # generate deterministic uuid based on reference_id
-    # so the generated uid is the same if the reference_id is the same
+def generate_deterministic_id(reference_id: str) -> uuid.UUID:
+    """Generate a deterministic UUID based on reference_id.
+    The generated UUID will be the same if the reference_id is the same.
+
+    Parameters
+    ----------
+    reference_id
+        A string representation of an UID
+
+    Returns
+    -------
+    uuid.UUID
+        The UUID object
+    """
     rng = random.Random(reference_id)
     uid = uuid.UUID(int=rng.getrandbits(128))
     return uid
