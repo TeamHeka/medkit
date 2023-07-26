c2bf58fe5d0b0c198db92b16da44480a5df836c0
Camila A <deycy-camila.arias-villamil@inria.fr>
2023-07-26 16:29:35 +0200

    Simplify mock function to generate doccano IDs

--- tests/unit/io/test_doccano_output_converter.py
+++ tests/unit/io/test_doccano_output_converter.py
@@ -15,19 +15,15 @@ _METADATA = {"custom_metadata": "custom", "doc_id": 1234}
 # mock of UUID class used by generate_deterministic_id
 @dataclasses.dataclass()
 class _MockUUID:
-    idx: int
-
-    @property
-    def int(self):
-        return self.idx
+    int: int
 
 
 @pytest.fixture(scope="module", autouse=True)
 def _mocked_generator_id(module_mocker):
-    # mock ids, force to be 0 or 1
+    # mock doccano IDs, force to 0 or 1 i.e. e0->0
     module_mocker.patch(
         "medkit.io.doccano.generate_deterministic_id",
-        lambda ref_id: _MockUUID(int(ref_id == "e2")),
+        lambda ref_id: _MockUUID(int(ref_id[1])),
     )
 
 
@@ -38,9 +34,9 @@ def _get_doc_by_task(task: DoccanoTask):
 
     if task == DoccanoTask.RELATION_EXTRACTION or task == DoccanoTask.SEQUENCE_LABELING:
         medkit_anns = [
-            Entity(label="ORG", spans=[Span(0, 6)], text="medkit", uid="e1"),
-            Entity(label="DATE", spans=[Span(22, 26)], text="2022", uid="e2"),
-            Relation(label="created_in", source_id="e1", target_id="e2", uid="r1"),
+            Entity(label="ORG", spans=[Span(0, 6)], text="medkit", uid="e0"),
+            Entity(label="DATE", spans=[Span(22, 26)], text="2022", uid="e1"),
+            Relation(label="created_in", source_id="e0", target_id="e1", uid="r0"),
         ]
 
         for ann in medkit_anns:
