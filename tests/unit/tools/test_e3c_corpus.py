50a904580ee90ba3103d9f8c96dc145854a385b6
HUYNH Kim-Tam <kim-tam.huynh@inria.fr>
2023-08-11 09:58:07 +0200

    [e3c corpus] add tests for annotation doc

--- tests/unit/tools/test_e3c_corpus.py
+++ tests/unit/tools/test_e3c_corpus.py
@@ -3,6 +3,8 @@ import pytest
 from medkit.tools.e3c_corpus import (
     convert_data_collection_to_medkit,
     load_data_collection,
+    convert_data_annotation_to_medkit,
+    load_data_annotation,
 )
 from medkit.io.medkit_json import load_text_documents
 
@@ -27,3 +29,16 @@ def test_convert_data_collection_to_medkit(e3c_corpus_path, tmpdir):
     docs_from_corpus = load_data_collection(dir_path=e3c_corpus_path, keep_id=True)
     docs_from_medkit = load_text_documents(medkit_file)
     assert list(docs_from_corpus)[0] == list(docs_from_medkit)[0]
+
+
+def test_convert_data_annotation_to_medkit(e3c_corpus_path, tmpdir):
+    medkit_file = tmpdir / "medkit.jsonl"
+    convert_data_annotation_to_medkit(
+        dir_path=e3c_corpus_path, output_file=medkit_file, keep_id=True
+    )
+    docs_from_corpus = load_data_annotation(dir_path=e3c_corpus_path, keep_id=True)
+    docs_from_medkit = load_text_documents(medkit_file)
+
+    assert len(list(docs_from_corpus)[0].anns.entities) == len(
+        list(docs_from_medkit)[0].anns.entities
+    )
