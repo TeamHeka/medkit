3eb3403e8bbea1b2450b95fea7b6c4dced543f27
HUYNH Kim-Tam <kim-tam.huynh@inria.fr>
2023-09-26 17:07:57 +0200

    [audio] fix check of pyannote.audio import

--- medkit/audio/segmentation/__init__.py
+++ medkit/audio/segmentation/__init__.py
@@ -5,5 +5,7 @@ from medkit.core.utils import modules_are_available
 if modules_are_available(["webrtcvad"]):
     __all__.append("webrtc_voice_detector")
 
-if modules_are_available(["torch", "pyannote.audio"]):
+if modules_are_available(["pyannote"]) and modules_are_available(
+    ["torch", "pyannote.audio"]
+):
     __all__.append("pa_speaker_detector")
